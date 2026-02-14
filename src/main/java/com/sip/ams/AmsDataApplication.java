3b1005006bbdea0163296663ee6dd33000d07487
safwen <safwene.essayes@ensi-uma.tn>
2026-02-14 01:37:33 +0100

    amsdataapp

--- src/main/java/com/sip/ams/AmsDataApplication.java
+++ src/main/java/com/sip/ams/AmsDataApplication.java
@@ -12,11 +12,7 @@ public class AmsDataApplication {
 	public static void main(String[] args) {
 		Etudiant e = new Etudiant();
 		new File(ArticleController.uploadDirectory).mkdir();  //création du dossier sous static
-		// PATCH Jenkins: Correction automatique du nom de classe dans SpringApplication.run
-        // Avant : SpringApplication.run(amsDataApplication.class, args);
-        // Après : SpringApplication.run(AmsDataApplication.class, args);
-        // Recommandation : Le nom de classe commence toujours par une majuscule.
-        SpringApplication.run(AmsDataApplication.class, args);
+		SpringApplication.run(amsDataApplication.class, args);
 		System.out.println("");
 
 	}
